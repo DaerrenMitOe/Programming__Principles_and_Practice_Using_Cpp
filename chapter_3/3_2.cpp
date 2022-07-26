2895d5babf38a52461299b6a28a66e375ba752e1
DaerrenMitOe <darrenketiamen@gmail.com>
2022-07-27 01:21:22 +0200

    Changes to be committed: 	modified:   .vscode/c_cpp_properties.json 	new file:   3_2.o 	modified:   chapter_2/hello_world 	new file:   chapter_3/3.1 	new file:   chapter_3/3.3 	new file:   chapter_3/3_2 	new file:   chapter_3/3_2.cpp 	new file:   chapter_3/3_3_1.cpp 	new file:   chapter_3/3_3_2 	new file:   chapter_3/3_3_2.cpp 	new file:   chapter_3/3_3_3 	new file:   chapter_3/3_3_3.cpp 	new file:   chapter_3/3_3_try_this 	new file:   chapter_3/3_3_try_this.cpp 	new file:   chapter_3/read_and_write_a_first_name.cpp
    Changes not staged for commit:
    	deleted:    3_2.o

---
+++ chapter_3/3_2.cpp
@@ -0,0 +1,12 @@
+#include "../std_lib_facilities.h"
+
+int main()
+{
+    // string name2 = 39; // error: 39 isn't a string
+    // int number_of_steps = 39; // error "Annemarie" is not an int
+
+    double flying_time = 3.5; // int for integers
+    char decimal_point = '.'; // double for floating-point numbers
+    string name = "Annemarie"; // char for individual characters
+    bool tap_on = true; // bool for logical variables
+}
