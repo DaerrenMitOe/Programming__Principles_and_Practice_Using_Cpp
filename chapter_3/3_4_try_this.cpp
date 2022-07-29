b3ebdb6a86b8da33d2b7fa906c599165657e3d10
DaerrenMitOe <69485693+DaerrenMitOe@users.noreply.github.com>
2022-07-29 22:59:56 +0200

    Changes to be committed: 	modified:   chapter_3/3_4_try_this.cpp 	new file:   chapter_3/3_5_1_try_this.cpp 	new file:   chapter_3/3_7_try_this.cpp 	new file:   chapter_3/3_9_2_try_this.cpp

--- chapter_3/3_4_try_this.cpp
+++ chapter_3/3_4_try_this.cpp
@@ -8,13 +8,13 @@ int main()
     double m;
     m = n;
     cout << "n == " << n
-    << "\nn + 1 == " << n + 1
-    << "\nn - 1 == " << n - 1
-    << "\nthree times n == " << 3 * n
-    << "\ntwice n == " << n + n
-    << "\nn squared == " << n * n
-    << "\nhalf of n == " << m / 2
-    << "\nmodulo of n == " << n % 2
-    << "\nsquare root of n == " << sqrt(m)
-    << '\n';
+         << "\nn + 1 == " << n + 1
+         << "\nn - 1 == " << n - 1
+         << "\nthree times n == " << 3 * n
+         << "\ntwice n == " << n + n
+         << "\nn squared == " << n * n
+         << "\nhalf of n == " << m / 2
+         << "\nmodulo of n == " << n % 2
+         << "\nsquare root of n == " << sqrt(m)
+         << '\n';
 }
