f2fe15442882a99e3e6587b69193436e7fdcb377
DaerrenMitOe <69485693+DaerrenMitOe@users.noreply.github.com>
2022-08-19 04:07:18 +0200

    Changes to be committed: 	modified:   .vscode/default.code-snippets 	modified:   chapter_4/drill_7.cpp 	new file:   chapter_4/drill_8.cpp

--- chapter_4/drill_8.cpp
+++ chapter_4/drill_8.cpp
@@ -9,31 +9,42 @@ int main()
     constexpr double in_to_cm = 2.54;
     constexpr double ft_to_in = 12;
 
+    cout << "Enter  \n";
+
     while (cin >> val >> unit)
     {
         if (unit == "cm")
         {
             cout << val << unit << " == " << val / m_to_cm << "m\n"
                  << val << unit << " == " << val / in_to_cm << "in\n"
-                 << val << unit << " == " << val / in_to_cm / ft_to_in << "ft\n\n";
+                 << val << unit << " == " << val / in_to_cm / ft_to_in << "ft\n\n"
+                 << "Enter  \n";
         }
         else if (unit == "m")
         {
             cout << val << unit << " == " << val * m_to_cm << "cm\n"
                  << val << unit << " == " << val * m_to_cm / in_to_cm << "in\n"
-                 << val << unit << " == " << val * m_to_cm / in_to_cm / ft_to_in << "ft\n\n";
+                 << val << unit << " == " << val * m_to_cm / in_to_cm / ft_to_in << "ft\n\n"
+                 << "Enter  \n";
         }
         else if (unit == "in")
         {
             cout << val << unit << " == " << val * in_to_cm << "cm\n"
                  << val << unit << " == " << val * in_to_cm / m_to_cm << "m\n"
-                 << val << unit << " == " << val / ft_to_in << "ft\n\n";
+                 << val << unit << " == " << val / ft_to_in << "ft\n\n"
+                 << "Enter  \n";
         }
         else if (unit == "ft")
         {
             cout << val << unit << " == " << val * ft_to_in * in_to_cm << "cm\n"
                  << val << unit << " == " << val * ft_to_in * in_to_cm / m_to_cm << "m\n"
-                 << val << unit << " == " << val * ft_to_in << "in\n\n";
+                 << val << unit << " == " << val * ft_to_in << "in\n\n"
+                 << "Enter  \n";
+        }
+        else
+        {
+            cout << "unknow unit\n\n"
+                 << "Enter  \n";
         }
     }
 
